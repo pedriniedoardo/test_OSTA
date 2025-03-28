6677de7ddd17063935374307c6e4db9df6e74b88
pedriniedoardo <pedrini.edoardo@gmail.com>
2025-03-28 17:23:46 +0100

    fix script

--- scr/99_test_shard_converter.R
+++ scr/99_test_shard_converter.R
@@ -1,7 +1,8 @@
 # libraries ---------------------------------------------------------------
 library(schard)
 library(tidyverse)
-# library(loupeR)
+library(Seurat)
+library(loupeR)
 
 # download the sample files -----------------------------------------------
 # download.file('https://datasets.cellxgene.cziscience.com/c5ac5c36-f60c-4680-8018-2d6cb65c0a37.h5ad','../../data/vis.heart.h5ad')
@@ -20,10 +21,10 @@ visx01 <- schard::h5ad2seurat_spatial('../../data/vis.heart.h5ad')
 visx02 <- schard::h5ad2seurat_spatial('../../data/vis.heart.h5ad',use.raw = T)
 
 # see the difference in loading with or without raw parameter
-visx01[["Spatial"]]$counts
-visx02[["Spatial"]]$counts
+visx01[["Spatial"]]$counts[1:10,1:10]
+visx02[["Spatial"]]$counts[1:10,1:10]
 
-# or load as list of Seurat objects (per slide
+# or load as list of Seurat objects (per slide)
 visl <- schard::h5ad2seurat_spatial('../../data/vis.heart.h5ad',simplify = FALSE)
 
 # or load raw counts
@@ -33,8 +34,8 @@ snhr <- schard::h5ad2seurat('../../data/sn.heart.h5ad',use.raw = TRUE)
 visr <- schard::h5ad2seurat_spatial('../../data/vis.heart.h5ad',use.raw = TRUE)
 
 # check that it works
-Seurat::SpatialPlot(visx,features = 'total_counts')
-Seurat::SpatialPlot(visx,features = 'total_counts',images = 'HCAHeartST11702009')
+Seurat::SpatialPlot(visx01,features = 'total_counts')
+Seurat::SpatialPlot(visx01,features = 'total_counts',images = 'HCAHeartST11702009')
 Seurat::SpatialPlot(visl$HCAHeartST11702010,features = 'total_counts')
 
 # raw counts are different from normolized ones
